d6df08b524831c7588df96a0ea210d5de9fb32a3
glebsamsonov-nbcuni <Gleb.Samsonov@nbcuni.com>
2020-09-07 16:58:40 +0200

    init

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -10,6 +10,14 @@ class Pipeline {
     }
 
     def execute() {
+//    ===================== Your Code Starts Here =====================
+//    Note : use "script" to access objects from jenkins pipeline run (WorkflowScript passed from Jenkinsfile)
+//           for example: script.node(), script.stage() etc
 
+//    ===================== Parse configuration file ==================
+
+//    ===================== Run pipeline stages =======================
+
+//    ===================== End pipeline ==============================
     }
 }
