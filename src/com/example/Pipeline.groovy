5a4cb8f12080a285b0d76ca395d90f12dfd365f2
temtestuser <44535823+temtestuser@users.noreply.github.com>
2020-11-20 15:52:22 -0800

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -26,7 +26,7 @@ class Pipeline {
             //def content = script.readFile("${configurationFile}")
             //script.println(content)
             def yml = script.readYaml file: "${configurationFile}"
-            script.println(yml)
+            script.println(yml.notifications)
             
         script.stage('test'){
         script.echo "hi"
