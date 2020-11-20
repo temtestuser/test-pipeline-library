165ed4dac8e9c3b3372157b2bd48c59ddb8a2c81
temtestuser <44535823+temtestuser@users.noreply.github.com>
2020-11-20 12:26:07 -0800

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -20,7 +20,8 @@ class Pipeline {
 
 //    ===================== End pipeline ==============================
         script.node(){
-            //Map pipelineCfg = script.readFile(file: "../${configurationFile}")
+            Map pipelineCfg = script.readFile(file: "script.${configurationFile}")
+        
         script.stage('test'){
         script.echo "hi"
         }
