4d2b45bf4445420d79e4b4690953133b317d7199
temtestuser <44535823+temtestuser@users.noreply.github.com>
2020-11-19 17:12:15 -0800

    Update Pipeline.groovy

--- src/com/example/Pipeline.groovy
+++ src/com/example/Pipeline.groovy
@@ -19,7 +19,7 @@ class Pipeline {
 //    ===================== Run pipeline stages =======================
 
 //    ===================== End pipeline ==============================
-        node{
+        script.stage('Hi'){
         echo "hello"
         }
     }
